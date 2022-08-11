cf81a6eeb1af7ec34955fc5a708f3de116950185
saulgame2010 <saulgame2010@gmail.com>
2022-08-10 21:39:21 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Cascarón para aplicación con webpack
+
+Esta es una aplicación que usa webpack y ya tiene configuraciones básicas
+
+## Notas
+
+Recuerden reconstruir los módulos de Node con:
+
+```bash
+
+npm install
+
+```
---
+++ src/js/componentes.js
@@ -0,0 +1,34 @@
+import { Todo } from "../classes";
+import { todoList } from "../index";
+
+// Referencias al HTML
+const divTodoList = document.querySelector('.todo-list');
+const txtInput = document.querySelector('.new-todo');
+
+export const crearTodoHtml = (todo) => {
+    const htmlTodo = `
+    <li class="${todo.completado ? 'completed' : ''}" data-id="${todo.id}">
+		<div class="view">
+			<input class="toggle" type="checkbox" ${todo.completado ? 'checked' : ''}>
+			<label>${todo.tarea}</label>
+			<button class="destroy"></button>
+		</div>
+		<input class="edit" value="Create a TodoMVC template">
+	</li>
+    `;
+    const div = document.createElement('div');
+    div.innerHTML = htmlTodo;
+    divTodoList.append(div.firstElementChild);
+    return div;
+}
+
+// Eventos
+txtInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13 && txtInput.value.length > 0) {
+        console.log(txtInput.value);
+        const nuevoTodo = new Todo(txtInput.value);
+        todoList.nuevoTodo(nuevoTodo);
+        crearTodoHtml(nuevoTodo);
+        txtInput.value = '';
+    }
+});
